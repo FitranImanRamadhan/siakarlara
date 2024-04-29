f7221eb8e74dc06a84c6782c6f74a6ba1e70d280
FitranImanRamadhan <fitraniman11@gmail.com>
2024-04-29 16:05:30 +0700

    tmp

--- resources/views/penggajians/create.blade.php
+++ resources/views/penggajians/create.blade.php
@@ -4,37 +4,39 @@
     <br>
     <form action="{{ route('penggajians.create') }}" method="GET" id="attendanceForm">
         @csrf
-        <div class="mb-3">
-            <label for="tahun" class="form-label">Tahun</label>
-            <select class="form-select" id="tahun" name="tahun">
-                <option selected disabled>Select Tahun</option>
-                @for ($i = date('Y'); $i >= 2010; $i--)
-                    <option value="{{ $i }}">{{ $i }}</option>
-                @endfor
-            </select>
-        </div>
-        <div class="mb-3">
-            <label for="bulan" class="form-label">Bulan</label>
-            <select class="form-select" id="bulan" name="bulan">
-                <option selected disabled>Select Bulan</option>
-                @for ($i = 1; $i <= 12; $i++)
-                    <option value="{{ $i }}">{{ date('F', mktime(0, 0, 0, $i, 1)) }}</option>
-                @endfor
-            </select>
-        </div>
-        <button class="btn btn-primary" type="submit">Submit</button>
+        <div class="row">
+            <div class="col-md-3">
+                <label for="bulan" style="font-weight: bold;">Bulan:</label>
+                <select class="form-select" id="bulan" name="bulan">
+                    <option value="">Pilih Bulan</option>
+                    @for ($i = 1; $i <= 12; $i++)
+                        <option value="{{ $i }}">{{ date('F', mktime(0, 0, 0, $i, 1)) }}</option>
+                    @endfor
+                </select>
+            </div>
+            <div class="col-md-3">
+                <label for="tahun" style="font-weight: bold;">Tahun:</label>
+                <input class="form-control" id="tahun" name="tahun" value="{{ date('Y') }}" readonly>
+            </div>
+            
+            
+            <div class="col-md-2 align-self-end">
+                <button class="btn btn-primary" type="submit">Generate</button>
+            </div>
     </form>
     <br><br>
 
-    @if($absensis->isNotEmpty())
+    @if ($absensis->isNotEmpty())
         <form action="{{ route('penggajians.store') }}" method="POST" id="attendanceForm">
             @csrf
-            <input  name="tahun" value="{{ $tahun }}">
-            <input  name="bulan" value="{{ $bulan }}">
-            <div id="attendanceTable">
-                <p>Toleransi <span class="text-danger">15</span> menit</p>
+            <input type="hidden" name="tahun" value="{{ $tahun }}">
+            <input type="hidden" name="bulan" value="{{ $bulan }}">
+            <br>
+            <div id="attendanceTable" class="overflow-x-auto">
+                <p>Input Gaji Untuk bulan <span class="text-danger">{{ date('F', mktime(0, 0, 0, $bulan, 1)) }}</span> Tahun
+                    <span class="text-danger">{{ $tahun }}</span></p>
                 <div class="table-responsive">
-                    <table class="table table-bordered">
+                    <table class="table table-bordered" style="width: 200%;">
                         <thead>
                             <tr>
                                 <th>Nama</th>
@@ -43,6 +45,9 @@
                                 <th>Tunjangan</th>
                                 <th>Uang Makan</th>
                                 <th>Hadir</th>
+                                <th>Terlambat (L)</th>
+                                <th>Selisih (Q)</th>
+                                <th>Penjualan</th>
                                 <th>Total Gaji</th>
                                 <th>Gaji Kotor</th>
                                 <th>BPJS Kerja</th>
@@ -61,23 +66,34 @@
                                     <td>{{ $absensi->pegawai->position->tunjangan_jabatan }}</td>
                                     <td>{{ $absensi->pegawai->position->uang_makan }}</td>
                                     <td>{{ $absensi->hadir }}</td>
+                                    <td>{{ $absensi->terlambat }}</td>
+                                    <td>{{ $absensi->selisih }}</td>
+                                    <td>{{ $absensi->Penjualan }}</td>
                                     <td>
                                         <input type="hidden" name="absensi_id[]" value="{{ $absensi->id }}">
-                                        <input class="form-control" type="number" name="total_gaji[]" placeholder="Total Gaji" required>
+                                        <input class="form-control" type="number" name="total_gaji[]"
+                                            value="{{ $absensi->hadir * $absensi->pegawai->position->gaji_perhari }}" 
+                                            placeholder="Total Gaji" required readonly>
                                     </td>
-                                    <td><input class="form-control" type="number" name="gaji_kotor[]" placeholder="Gaji Kotor" required></td>
-                                    <td><input class="form-control" type="number" name="bpjs_tk[]" placeholder="BPJS TK" required></td>
-                                    <td><input class="form-control" type="number" name="bpjs_kes[]" placeholder="BPJS Kes" required></td>
-                                    <td><input class="form-control" type="number" name="gaji_bersih[]" placeholder="Gaji Bersih" required></td>
-                                    <td><input class="form-control" type="number" name="pembulatan[]" placeholder="Pembulatan" required></td>
-                                    <td><input class="form-control" type="number" name="gaji_diterima[]" placeholder="Gaji Diterima" required></td>
+                                    <td><input class="form-control" type="number" name="gaji_kotor[]"
+                                            placeholder="Gaji Kotor" required></td>
+                                    <td><input class="form-control" type="number" name="bpjs_tk[]" placeholder="BPJS TK"
+                                            required></td>
+                                    <td><input class="form-control" type="number" name="bpjs_kes[]" placeholder="BPJS Kes"
+                                            required></td>
+                                    <td><input class="form-control" type="number" name="gaji_bersih[]"
+                                            placeholder="Gaji Bersih" required></td>
+                                    <td><input class="form-control" type="number" name="pembulatan[]"
+                                            placeholder="Pembulatan" required></td>
+                                    <td><input class="form-control" type="number" name="gaji_diterima[]"
+                                            placeholder="Gaji Diterima" required></td>
                                 </tr>
                             @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
-
+<br>
             <button class="btn btn-primary" type="submit">Submit</button>
         </form>
     @endif
